d970b547054632076e6123b7f29c03b9a9c70411
marcelo <marce@macbook-pro-2.lan>
2017-10-22 19:54:35 -0300

    Agregado de los archivos de ejemplo

---
+++ src/NumberToEnglish.java
@@ -0,0 +1,8 @@
+public class NumberToEnglish {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World !");
+        String sayit = new ComplexMethod().intToEnglishValue(3);
+        System.out.println(sayit);
+    }
+}
